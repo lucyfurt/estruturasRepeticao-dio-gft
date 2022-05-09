5af79dfd93eb78164da86f30029b21a1b2cc25bb
lucyfurt <lucianafurtado10@gmail.com>
2022-05-09 15:22:10 -0300

    fatorial

--- src/Tests/Fatorial.java
+++ src/Tests/Fatorial.java
@@ -1,4 +1,16 @@
 package Tests;
 
+import java.util.Scanner;
+
 public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("fatorial");
+        int fatorial = scan.nextInt();
+        int multiplicacao = 1;
+        for(int i = fatorial; i>=1; i--){
+            multiplicacao = multiplicacao * i;
+        }
+        System.out.println(fatorial +"! =" + multiplicacao);
+    }
 }
